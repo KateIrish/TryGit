37fa7676f4f70a6d207f7cd02af09798af707ede
student <student@L411-S28>
2018-09-28 12:34:24 +0800

    Added lines of codes that displays base raise to exponent

--- src/git/Git.java
+++ src/git/Git.java
@@ -1,11 +1,30 @@
-
 package git;
-
 
 public class Git {
 
-    public static void main(String[] args) {
-        System.out.println("Try Git");
+    /**
+     *
+     * @param base is the base number that would be repeated
+     * @param exponent will serve as the exponent
+     * @return
+     */
+    public static double power(double base, int exponent) {
+        double yield = 1;
+        
+        for (int counter = 0; counter < exponent; counter++) {
+            if (counter != exponent) {
+                yield = yield * base;
+               
+            }
+
+        }
+         System.out.println(yield);
+         return yield;
     }
-    
+
+    public static void main(String[] args) {
+        power(2, 3);
+
+    }
+
 }
